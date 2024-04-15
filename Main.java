d46b67ab1a962e92d602a467aeeedbb09c6b05e2
m0lmin <162698302+m0lmin@users.noreply.github.com>
2024-04-15 20:08:24 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    int a=23;
+    int b=10;
+    System.out.println ((double)a/b);
+    System.out.println (a%b);
+    int c=77;
+    System.out.println(c/10+c/10);
+
+    double d=1.7;
+    double e=1.2;
+    System.out.println((int)(d+0.5));
+    System.out.println((int)(e+0.5));
+    }
+}
